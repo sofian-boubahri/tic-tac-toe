16dc3be51761c0a4570aa855e3d59ec884ba7670
Sofian <sofianboubahri@gmail.com>
2024-12-03 15:32:17 +0100

    Initialisation du tableau en 3x3

---
+++ morpion.py
@@ -0,0 +1,10 @@
+morpion = [""] * 9
+
+def afficher_plateau(): 
+    print(f"| {morpion[0]} | {morpion[1]} | {morpion[2]} |")
+    print("-----------")
+    print(f"| {morpion[3]} | {morpion[4]} | {morpion[5]} |")
+    print("-----------")
+    print(f"| {morpion[6]} | {morpion[7]} | {morpion[8]} |")
+afficher_plateau()
+
